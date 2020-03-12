e647566516b149b54a0f037012e584362687418c
tao.haiwu <tao.haiwu@ofilm.com>
2020-03-12 09:45:05 +0800

    commit build script

---
+++ build/build.sh
@@ -0,0 +1,139 @@
+#!/bin/bash
+
+#prepare
+#setup environment
+
+. ./envsetup.sh
+
+subdir="${GSTREAMER_ROOT_SOURCE_PATH}/gstreamer-1.2.3  \
+		${GSTREAMER_ROOT_SOURCE_PATH}/gst-plugins-base-1.2.3 \
+		${GSTREAMER_ROOT_SOURCE_PATH}/gst-plugins-good-1.2.3 \
+		${GSTREAMER_ROOT_SOURCE_PATH}/gst-plugins-bad-1.2.3 \
+		${GSTREAMER_ROOT_SOURCE_PATH}/gst-canbuf \
+		${GSTREAMER_ROOT_SOURCE_PATH}/gst-isomp4 \
+		${GSTREAMER_ROOT_SOURCE_PATH}/gst-multifile \
+		${GSTREAMER_ROOT_SOURCE_PATH}/gst-ti-ducati" \
+
+if(($#==1)) ; then
+    if [ "$1"x = "clean"x ] ; then
+        for dir in ${subdir}; do
+			pushd ${dir}
+			make clean
+			popd
+		done		
+        exit $?
+    fi
+fi
+
+
+echo "--------------start build gstreamer----------------"
+./build_gstreamer.sh
+
+err_code=$?
+if [ "$err_code" != "0" ]
+then
+	echo "Build Gstreamer Failed, $err_code"
+	exit $err_code
+fi
+
+echo "--------------start build plugin base----------------"
+./build_plugin_base.sh
+
+err_code=$?
+if [ "$err_code" != "0" ]
+then
+	echo "Build base plugin Failed, $err_code"
+	exit $err_code
+fi
+
+./build_plugin_good.sh
+
+err_code=$?
+if [ "$err_code" != "0" ]
+then
+	echo "Build good plugin Failed, $err_code"
+	exit $err_code
+fi
+
+./build_plugin_bad.sh
+
+err_code=$?
+if [ "$err_code" != "0" ]
+then
+	echo "Build bad plugin Failed, $err_code"
+	exit $err_code
+fi
+
+./build_misc.sh
+
+err_code=$?
+if [ "$err_code" != "0" ]
+then
+	echo "Build misc Failed, $err_code"
+	exit $err_code
+fi
+
+#generate SDK
+MY_PATH=`pwd`
+SDK_PATH=${MY_PATH}/../${HOST}
+
+if [ -d ${SDK_PATH} ]
+then
+	rm -rf ${SDK_PATH}
+	mkdir -p ${SDK_PATH}/bin
+	mkdir -p ${SDK_PATH}/lib
+	mkdir -p ${SDK_PATH}/include
+else
+	mkdir -p ${SDK_PATH}/bin
+	mkdir -p ${SDK_PATH}/lib
+	mkdir -p ${SDK_PATH}/include
+fi
+
+cp -rf ${INSTALL_PATH}/bin/gst-* ${SDK_PATH}/bin/
+
+err_code=$?
+	if [ "$err_code" != "0" ]
+	then
+		echo "copy binary failed, $err_code"
+		exit $err_code
+	fi
+
+
+cp -rf ${INSTALL_PATH}/lib/libgst* ${SDK_PATH}/lib/
+
+err_code=$?
+	if [ "$err_code" != "0" ]
+	then
+		echo "copy libs failed, $err_code"
+		exit $err_code
+	fi
+
+cp -rf ${INSTALL_PATH}/lib/gstreamer-1.0 ${SDK_PATH}/lib/
+
+err_code=$?
+	if [ "$err_code" != "0" ]
+	then
+		echo "copy plug-ins failed, $err_code"
+		exit $err_code
+	fi
+	
+cp -rf ${INSTALL_PATH}/lib/gstreamer1.0 ${SDK_PATH}/lib/
+
+err_code=$?
+	if [ "$err_code" != "0" ]
+	then
+		echo "copy scaner failed, $err_code"
+		exit $err_code
+	fi
+	
+cp -rf ${INSTALL_PATH}/include/gstreamer-1.0 ${SDK_PATH}/include/
+
+err_code=$?
+	if [ "$err_code" != "0" ]
+	then
+		echo "copy header files failed, $err_code"
+		exit $err_code
+	fi
+	
+	
+	
---
+++ build/envsetup.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+##
+##
+##COMPILE tools
+##
+##
+#PREFIX=/opt/arm-toolchain/linux/linaro/gcc-linaro-arm-linux-gnueabihf-4.7/bin/arm-linux-gnueabihf-
+PREFIX=/home/taohaiwu/works/project/adas/c211/ti_components/os_tools/linux/linaro/gcc-linaro-5.3-2016.02-x86_64_arm-linux-gnueabihf/bin/arm-linux-gnueabihf-
+AS=${PREFIX}gcc
+CC=${PREFIX}gcc
+CXX=${PREFIX}g++
+LD=${PREFIX}ld
+AR=${PREFIX}ar
+RANLIB=${PREFIX}ranlib
+SIZE=${PREFIX}size
+STRIP=${PREFIX}strip
+CONV=${PREFIX}conv
+OBJDUMP=${PREFIX}objdump
+
+export CC
+export NM
+export STRIP
+export AR
+export RANLIB
+
+##
+##CPU options
+##
+CPUFLAGS="-mcpu=cortex-a15 -mtune=cortex-a15 -mfloat-abi=hard -mfpu=neon -marm"
+
+##
+## asm options
+##
+ASFLAGS=${CPUFLAGS}
+
+##
+## gcc options
+## 
+export CFLAGS=${CPUFLAGS}
+
+##
+## plugin configure options
+## 
+
+CURRENT_PATH=`pwd`
+
+VERSION=1.2.3
+SYS_ROOT_DIR=${CURRENT_PATH}/../../../rootfs/common/targetfs
+HOST=arm-linux-gnueabihf
+INSTALL_PATH=${SYS_ROOT_DIR}/usr
+
+DVR_TOP=${CURRENT_PATH}/..
+GSTREAMER_ROOT_SOURCE_PATH=${DVR_TOP}/gstreamer
+SDK_SOURCE_PATH=${DVR_TOP}/dvr_sdk
+SPECIFIED_INCLUDE_PATH=${SYS_ROOT_DIR}/usr/include
+SPECIFIED_LIB_PATH=${SYS_ROOT_DIR}/usr/lib
+
+export PKG_CONFIG_PATH=${SYS_ROOT_DIR}/usr/lib/pkgconfig
+export PATH=$PATH:/home/taohaiwu/works/project/adas/c211/ti_components/os_tools/linux/linaro/gcc-linaro-5.3-2016.02-x86_64_arm-linux-gnueabihf/bin/
